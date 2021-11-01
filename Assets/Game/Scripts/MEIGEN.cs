83b992e93cad6c9862365ed40f7fd5fdfc0d8f3e
yuta_akaike <yuta_0814@outlook.jp>
2021-11-01 19:32:58 +0900

    [add] 強化UIとその中身を追加

---
+++ Assets/Game/Scripts/MEIGEN.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MEIGEN : MonoBehaviour
+{
+    [SerializeField] private string[] meigen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
