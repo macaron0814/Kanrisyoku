3071ba028976f6c9fa610cd5eb420c35c312c789
yuta_akaike <yuta_0814@outlook.jp>
2021-12-06 14:42:42 +0900

    [add] パラメーター調整

--- Assets/Game/Scripts/Ramen.cs
+++ Assets/Game/Scripts/Ramen.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Ramen : MonoBehaviour
 {
-    [Header("スタミナN%消費で生成")]
+    [Header("残りHP:N%で生成")]
     [SerializeField] private float n = 20;
 
     // Start is called before the first frame update
     void Start()
     {
         float createRamenValue = Parameter.save.hpValue * (n / 100.0f);
-        if (ItemSystem.ramen > (100 - createRamenValue)) { gameObject.SetActive(false); }
+        if (ItemSystem.ramen > createRamenValue) { gameObject.SetActive(false); }
     }
 }
